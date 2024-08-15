941c7f4a0a83fec076af353fd339f144dfd2aedb
Moh. Rafli Dwi Saputra <133976925+Rap404@users.noreply.github.com>
2024-08-15 16:31:13 +0700

    update render tasks

--- resources/js/Pages/Project/Show.jsx
+++ resources/js/Pages/Project/Show.jsx
@@ -1,8 +1,9 @@
 import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constant";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
+import TasksTable from "../Task/TasksTable";
 
-export default function Show({ auth, project }) {
+export default function Show({ auth, project, tasks, queryParams }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -17,14 +18,14 @@ export default function Show({ auth, project }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div>
+                            <img
+                                src={project.image_path}
+                                alt=""
+                                className="w-full h-64 object-cover"
+                            />
+                        </div>
                         <div className="p-6 text-gray-900 dark:text-gray-100">
-                            <div>
-                                <img
-                                    src={project.image_path}
-                                    alt=""
-                                    className="w-full h-64 object-cover"
-                                />
-                            </div>
                             <div className="grid gap-1 grid-cols-2 mt-2">
                                 <div>
                                     <div>
@@ -97,6 +98,26 @@ export default function Show({ auth, project }) {
                                     </div>
                                 </div>
                             </div>
+                            <div className="mt-4">
+                                <label className="font-bold text-lg">
+                                    Project Description
+                                </label>
+                                <p className="mt-1">{project.description}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="pb-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900 dark:text-gray-100">
+                            <TasksTable
+                                tasks={tasks}
+                                queryParams={queryParams}
+                                hideProjectColumn={true}
+                            />
                         </div>
                     </div>
                 </div>
